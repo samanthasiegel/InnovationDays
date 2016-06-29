e760b33eb00d258ca03a480f1985398b57da919e
samanthasiegel <sps27@duke.edu>
2016-06-29 09:30:16 -0700

    Still can't fill out check boxes!

--- server.js
+++ server.js
@@ -23,6 +23,97 @@ app.use(busboy());
 var path = require('path');
 var fs = require('fs');
 
+//email 
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var transporter = nodemailer.createTransport(smtpTransport({
+	service: 'gmail',
+	auth: {
+		user: 'samanthasiegel96@gmail.com',
+		pass: 'August81996!'
+	}
+}));
+
+var mailOptions = {
+	from: '"Sammi Siegel" <samanthasiegel96@gmail.com>',
+	to: 'sps27@duke.edu',
+	subject: 'Test',
+	text: 'Just testing',
+	html: '<b> Just testing </b>',
+	attachments: [{
+		filename: '',//file name here 
+		path: '', //path to file --> taken from form
+		contentType: 'application/pdf'
+	}], function(err, info){
+		if(err){
+			console.log(err);
+		} else{
+			console.log(info);
+		}
+	}
+};
+
+//email handler
+app.get('/test-mail', function(req, res){
+	res.render('test-mail');
+});
+
+app.post('/test-mail', function(req, res){
+	transporter.sendMail(mailOptions, function(err, info){
+		if(err) return console.log(err);
+		console.log('Message sent: ' + info.response);
+	});
+	res.render('submit');
+});
+
+// pdf stuff
+var pdfFiller = require('pdffiller');
+
+var sourcePDF = "final-ACH.pdf";
+var destinationPDF =  "output-ACH.pdf";
+var shouldFlatten = true;
+var data = {
+    "banking_institution" : "Chase",
+    "home_address" : "123 Lit Lane",
+    "routing_number" : "123456789",
+    "account_number" : "123456789",
+    "account_type" : "MoneyJ$",
+    "name_on_account" : "Sammi Node Siegel",
+    "name_printed" : "Sammi",
+    "name_signature" : "Sammi Sign Here",
+    "date" : "06/28/2016",
+    "phone" : "123-456-7890",
+    "email" : "sammi@intuit.com"
+};
+ 
+pdfFiller.fillForm( sourcePDF, destinationPDF, data, function(err) {
+    if (err) throw err;
+    console.log("In callback (we're done).");
+});
+
+var sourcePDFTax = "final-TAX.pdf";
+var destinationPDFTax =  "output-TAX.pdf";
+var dataTax = {
+    "employee_name" : "Sammi",
+    "monthly_salary" : "$$$",
+    "yes_tax" : "1",
+    "no_tax" : "1",
+    "single" : "1",
+    "filing_jointly" : "1",
+    "filing_separately" : "1",
+    "head_household" : "1",
+    "yourself" : "1",
+    "spouse" : "1",
+    "dep_16_under" : "1",
+    "dep_17_over" : "1",
+    "total_exemptions" : "1",
+    "auth_sig" : "Sign here"
+};
+ 
+pdfFiller.fillForm(sourcePDFTax, destinationPDFTax, dataTax, function(err) {
+    if (err) throw err;
+    console.log("In callback (we're done).");
+});
 
 //form page
 app.get('/', function(req, res){
@@ -58,6 +149,11 @@ app.post('/upload', function(req, res){
 app.get('/submit', function(req, res, next){
 	res.render('submit');
 })
+
+//test form UI
+app.get('/personal-info', function(req, res){
+	res.render('personal-info');
+});
 
 //run application
 app.listen(port);
